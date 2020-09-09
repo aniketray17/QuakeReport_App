bada2239f4d5bb6e3b64506537c43d656884f640
Aniket <46789080+aniketray17@users.noreply.github.com>
2020-09-09 09:08:43 +0530

    Added comments, updated the xml layout

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -20,13 +20,6 @@ public class EarthquakeActivity extends AppCompatActivity {
 
         // Create a fake list of earthquake locations.
         ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-        earthquakes.add(new Earthquake("7.2","San Francisco","Feb 2,2016"));
-        earthquakes.add(new Earthquake("6.1","London","July 20,2015"));
-        earthquakes.add(new Earthquake("3.9","Tokyo","Nov 10,2014"));
-        earthquakes.add(new Earthquake("5.4","Mexico City","May 3,2014"));
-        earthquakes.add(new Earthquake("2.8","Moscow","Jan 31,2013"));
-        earthquakes.add(new Earthquake("4.9","Rio de Janeiro","Aug 19,2012"));
-        earthquakes.add(new Earthquake("1.6","Paris","Oct 30,2011"));
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
