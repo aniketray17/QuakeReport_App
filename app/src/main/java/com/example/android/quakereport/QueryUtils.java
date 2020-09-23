00fea96e41ff9af81d7192ffab55b2a6060713a7
Aniket <46789080+aniketray17@users.noreply.github.com>
2020-09-23 09:26:08 +0530

    added progress bar

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -39,7 +39,11 @@ public final class QueryUtils {
      * Query the USGS dataset and return a list of {@link Earthquake} objects.
      */
     public static List<Earthquake> fetchEarthquakeData(String requestUrl) {
-        Log.i(LOG_TAG,"fetchEarthquakeData() called ...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         // Create URL object
         URL url = createUrl(requestUrl);
 
