71ed3fbd34820c4eea2d4b7fd6c264efe9777193
Aniket <46789080+aniketray17@users.noreply.github.com>
2020-09-24 08:15:56 +0530

    added new resources files

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -12,6 +12,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -141,5 +143,21 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
             // Loader reset, so we can clear out our existing data.
             Log.i(LOG_TAG,"onLoaderReset called ...");
             mAdapter.clear();
+        }
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == R.id.action_settings) {
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
         }
 }
